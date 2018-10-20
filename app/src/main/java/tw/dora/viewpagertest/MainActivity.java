cdd672fee45b9250644baa8910f439f9fba04f7d
kleinerQ <chih.yen5408@gmail.com>
2018-10-20 14:36:28 +0800

    v3

--- app/src/main/java/tw/dora/viewpagertest/MainActivity.java
+++ app/src/main/java/tw/dora/viewpagertest/MainActivity.java
@@ -1,13 +1,45 @@
 package tw.dora.viewpagertest;
 
+import android.graphics.Color;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private ViewPager viewPager;
+    private Fragment[] fs = new Fragment[3];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fs[0] = new Page1();
+        fs[1] = new Page2();
+        fs[2] = new Page3();
+
+        viewPager = findViewById(R.id.container);
+        viewPager.setAdapter(new MyPagerAdapter(getSupportFragmentManager()));
+
+    }
+
+    private class MyPagerAdapter extends FragmentStatePagerAdapter {
+
+        public MyPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int i) {
+            return fs[i];
+        }
+
+        @Override
+        public int getCount() {
+            return fs.length;
+        }
     }
 }
--- app/src/main/java/tw/dora/viewpagertest/Page3.java
+++ app/src/main/java/tw/dora/viewpagertest/Page3.java
@@ -11,7 +11,7 @@ public class Page3 extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_page3, container, false);
     }
 
